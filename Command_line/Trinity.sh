9fc616c5dfd039311a49a3dd9bcddaf169398517
boechera-rnaseq <53542649+boechera-rnaseq@users.noreply.github.com>
2019-08-07 16:28:35 -0700

    Update Trinity.sh

--- Command_line/Trinity.sh
+++ Command_line/Trinity.sh
@@ -11,6 +11,7 @@
 #SBATCH --job-name="trinity_arcuata"
 #SBATCH -p batch
 
+#load modules from hpcc
 module load trinity-rnaseq/2.6.6
 module load bowtie2
 
@@ -25,6 +26,8 @@ table_o_files="table_arcuata.txt"
 cpu="32"
 max_mem="280G"
 
+#necessary for --quality_trimming_params
+#in this case, used to ask trimmomatic to retain both reads of a pair, even if one is "contained" in the other
 TRIMMOMATIC_DIR="/bigdata/operations/pkgadmin/opt/linux/centos/7.x/x86_64/pkgs/trinity-rnaseq/2.6.6/trinity-plugins/Trimmomatic"
 
 $TRINITY_HOME/Trinity --trimmomatic --quality_trimming_params "ILLUMINACLIP:$TRIMMOMATIC_DIR/adapters/TruSeq3-PE.fa:2:30:10:4:TRUE SLIDINGWINDOW:4:5 LEADING:5 TRAILING:5 MINLEN:50" --seqType fq --SS_lib_type RF --samples_file $table_o_files --CPU $cpu --max_memory $max_mem --bflyCalculateCPU;
