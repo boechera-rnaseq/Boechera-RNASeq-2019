f61f6cb66b9a89a191f5541ccb8a824c84a1f03b
boechera-rnaseq <53542649+boechera-rnaseq@users.noreply.github.com>
2019-08-07 16:33:49 -0700

    Update stats.sh

--- Command_line/stats.sh
+++ Command_line/stats.sh
@@ -11,16 +11,18 @@
 #SBATCH --job-name="stats_Barc"
 #SBATCH -p batch
 
+#load modules from hpcc
 module load trinity-rnaseq
 module load bowtie2
 
+#create separate directory for output files
+mkdir stats
+
 group="Barc"
 align_file="../stats/bowtie2_Barc_align.txt"
-mkdir stats
 
 #stats for trinity assembly
 $TRINITY_HOME/util/TrinityStats.pl trinity_out_dir/Trinity.fasta > stats/trinity_stats_$group.txt;
-
 
 #get read alignment stats
 #there are a lot of intermediate files, keep them in trinity out dir
@@ -28,10 +30,16 @@ $TRINITY_HOME/util/TrinityStats.pl trinity_out_dir/Trinity.fasta > stats/trinity
 echo "starting bowtie2 analysis..."
     
 cd trinity_out_dir
-bowtie2-build Trinity.fasta Trinity_fasta
-    
-bowtie2 -x Trinity_fasta -1 Barc-C1_r1.fastq.gz.PwU.qtrim.fq,Barc-C2_r1.fastq.gz.PwU.qtrim.fq,Barc-C3_r1.fastq.gz.PwU.qtrim.fq,Barc-HS1_r1.fastq.gz.PwU.qtrim.fq,Barc-HS2_r1.fastq.gz.PwU.qtrim.fq,Barc-HS3_r1.fastq.gz.PwU.qtrim.fq -2 Barc-C1_r2.fastq.gz.PwU.qtrim.fq,Barc-C2_r2.fastq.gz.PwU.qtrim.fq,Barc-C3_r2.fastq.gz.PwU.qtrim.fq,Barc-HS1_r2.fastq.gz.PwU.qtrim.fq,Barc-HS2_r2.fastq.gz.PwU.qtrim.fq,Barc-HS3_r2.fastq.gz.PwU.qtrim.fq -q -k 20 --no-unal -p 10 2>$align_file | samtools view -@10 -Sb -o bowtie2.bam
 
+#index transcriptome for faster alignment
+bowtie2-build Trinity.fasta Trinity_fasta
+
+#running this will give total alignment rate for all reads
+#bowtie2 -x Trinity_fasta -1 Barc-C1_r1.fastq.gz.PwU.qtrim.fq,Barc-C2_r1.fastq.gz.PwU.qtrim.fq,Barc-C3_r1.fastq.gz.PwU.qtrim.fq,Barc-HS1_r1.fastq.gz.PwU.qtrim.fq,Barc-HS2_r1.fastq.gz.PwU.qtrim.fq,Barc-HS3_r1.fastq.gz.PwU.qtrim.fq -2 Barc-C1_r2.fastq.gz.PwU.qtrim.fq,Barc-C2_r2.fastq.gz.PwU.qtrim.fq,Barc-C3_r2.fastq.gz.PwU.qtrim.fq,Barc-HS1_r2.fastq.gz.PwU.qtrim.fq,Barc-HS2_r2.fastq.gz.PwU.qtrim.fq,Barc-HS3_r2.fastq.gz.PwU.qtrim.fq -q -k 20 --no-unal -p 10 2>$align_file | samtools view -@10 -Sb -o bowtie2.bam
+
+#exit 0
+
+#running this will give alignment for each set of reads
 group="Barc-C1"
 align_file="../stats/bowtie2_"$group"_stats.txt"
 
